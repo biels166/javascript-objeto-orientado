39571a3b153066b02927e747361528bba9971290
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-10-02 16:06:47 -0300

    Update script.js

--- carrinho de compras/script.js
+++ carrinho de compras/script.js
@@ -1,19 +1,37 @@
 class Compra{
-    constructor(item, quantidade, valor, total){
+    constructor(item, quantidade, valor){
         this.item = item;
         this.quantidade = quantidade;
         this.valor = valor;
-        this.total = quantidade * valor;
+    }
+
+    adicionar(item){
+        let contador = 0;
+
+        for(let itemCompra in this.item){
+            if(this.item[itemCompra].id == item.id){
+                this.item[itemCompra].qtd += item.qtd;
+            }
+            contador = 1;
+        }
+
+        if(contador == 0){
+            this.item.push(item);
+        }
+
+        this.quantidade = quantidade + item.qtd;
+        this.valor = valor + (item.preco * item.qtd);        
     }
 
 
 }
 
-let item1 = new Compra("Computador", 2, 2000);
+let compra = new Compra([{id: 01, nome: camisa, qtd: 2, preco: 50}, 2, 100]);
 
 
-console.log(item1.item);
-console.log(item1.quantidade);
-console.log(item1.valor);
 
-console.log(item1.total);
+
+
+
+
+
