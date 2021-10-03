f24b47f6b8e694980727037882933c79d5d07300
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-10-03 11:48:16 -0300

    update

---
+++ set endereço/script.js
@@ -0,0 +1,11 @@
+class Endereco{
+    constructor(rua, bairro, cidade, estado){
+        this.rua = rua;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    
+}
+
