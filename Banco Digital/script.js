2ee47bdbb7cf16f4cb8fb45f2db93e69672aaf3e
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-10-03 17:50:32 -0300

    Banco Digital

---
+++ Banco Digital/script.js
@@ -0,0 +1,8 @@
+class Conta{
+    constructor(saldoContaCorrente, saldoPoupanca, jurosPoupanca){
+        this.saldoContaCorrente = saldoContaCorrente;
+        this.saldoPoupanca = saldoPoupanca;
+        this.jurosPoupanca = jurosPoupanca;
+    }
+    
+}
